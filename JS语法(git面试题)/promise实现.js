c24e5dbcaa7c6c0b1bf48c890679ad384c9d5ddb
yuandh@g4b.cn <yuandh@g4b.cn>
2021-11-22 17:17:53 +0800

    promiose new js练习

---
+++ JS语法(git面试题)/promise实现.js
@@ -0,0 +1,53 @@
+// 三种状态
+const PENDING = "pending";
+const RESOLVED = "resolved";
+const REJECTED = "rejected";
+// promise 接收一个函数参数，该函数会立即执行
+function MyPromise(fn) {
+  let _this = this;
+  _this.currentState = PENDING;
+  _this.value = undefined;
+  // 用于保存 then 中的回调，只有当 promise
+  // 状态为 pending 时才会缓存，并且每个实例至多缓存一个
+  _this.resolvedCallbacks = [];
+  _this.rejectedCallbacks = [];
+
+  _this.resolve = function (value) {
+    if (value instanceof MyPromise) {
+      // 如果 value 是个 Promise，递归执行
+      return value.then(_this.resolve, _this.reject)
+    }
+    setTimeout(() => { // 异步执行，保证执行顺序
+      if (_this.currentState === PENDING) {
+        _this.currentState = RESOLVED;
+        _this.value = value;
+        _this.resolvedCallbacks.forEach(cb => cb());
+      }
+    })
+  };
+
+  _this.reject = function (reason) {
+    setTimeout(() => { // 异步执行，保证执行顺序
+      if (_this.currentState === PENDING) {
+        _this.currentState = REJECTED;
+        _this.value = reason;
+        _this.rejectedCallbacks.forEach(cb => cb());
+      }
+    })
+  }
+  // 用于解决以下问题
+  // new Promise(() => throw Error('error))
+  try {
+    fn(_this.resolve, _this.reject);
+  } catch (e) {
+    _this.reject(e);
+  }
+}
+
+let promise = new MyPromise((resolve,reject)=>{
+  resolve('2')
+})
+
+setTimeout(()=>{
+  console.log(promise)
+},1000)
